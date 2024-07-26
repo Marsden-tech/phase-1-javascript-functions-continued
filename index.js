b2b514b08fb786ad631f55e852f634563a0e8986
Marsden Mathews Odhiambo <marsdenlwero@gmail.com>
2024-07-26 12:38:30 +0300

    Added function

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    console.log(`This Saturday, I want to ${activity}`);
+}
+
+const mondayWork = function (activity = 'go to the office') {
+    console.log(`This Monday, I will ${activity}`);
+}
+
+function wrapAdjective(highlight = "*") {
+    return function (adjective = "special") {
+        return `You are ${highlight}${adjective}${highlight}!`;
+    };
+}
